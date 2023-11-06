f83371d68951a6d4127b55c395cebbe132de5102
Harsha Patel <109841543+HPK017@users.noreply.github.com>
2023-11-06 16:49:11 +0530

    Add files via upload

---
+++ Day4/dmo2.js
@@ -0,0 +1,6 @@
+var mainDiv = document.getElementById("mainDiv");
+        var paraElements = document.getElementsByClassName("para");
+        
+        for (var i = 0; i < paraElements.length; i++) {
+            paraElements[i].style.color = "red";
+        }
